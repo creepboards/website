9b7d11bd0ff281277401c2753bbe299b023c9a58
Maxwell Mullin <mullinmax@gmail.com>
2021-06-18 18:14:54 -0400

    clean up

--- components/component.py
+++
@@ -1,9 +0,0 @@
-class component():
-
-    def __init__(self) -> None:
-        self.state = {}
-        pass
-
-    def get_snippet(self, path):
-        with open(path, encoding="utf8") as f:
-            return f.read()
--- components/text.py
+++
@@ -1,9 +0,0 @@
-from components.component import component
-
-class text(component):
-    def __init__(self, content):
-        super().__init__()
-        self.state['content'] = content
-
-    def render(self):
-        return self.state['content']
