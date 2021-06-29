5b6706a9099d12f475faade446c014e5e8ece042
Maxwell Mullin <mullinmax@gmail.com>
2021-06-28 21:47:54 -0400

    much better nav with materialize css

--- public/js/pager.js
+++ public/js/pager.js
@@ -1,21 +1,37 @@
 // for each element with pager-container class
 Array.from(document.getElementsByClassName("pager-container")).forEach(
     function (container){
-        Array.from(container.children).forEach(
+        Array.from(container.children).forEach( 
             function (button){
-                // attach listener
+                // attach listener to each selector
                 button.addEventListener("click", function(){
-                    select_button(button);
-                    hide_all_pages(container);
-                    display_page(container, button.value);
-                    console.log('listening to:', button);
+                    // get all elements with the prefix
+                    var all_pages = document.querySelectorAll(`[id^="${button.parentNode.id}:"]`);
+                    
+                    // hide all but the selected page
+                    [].forEach.call(all_pages, function(page) {
+                        const target_id = `${button.parentNode.id}:${button.id}`
+                        if (page.id == target_id){
+                            page.hidden = false;
+                        } else {
+                            console.log(page.id, target_id)
+                            page.hidden = true;
+                        }
+                    });
+                    
+                    // activate the correct item
+                    var all_buttons = get_siblings(button)
+                    for(var i in all_buttons){
+                        all_buttons[i].classList.remove("active");
+                    }
+                    button.classList.add("active");
                 });
             }
         )
     }
 );
 
-let getSiblings = function (e) {
+let get_siblings = function (e) {
     // for collecting siblings
     let siblings = []; 
     // if no parent, return no sibling
@@ -34,29 +50,3 @@ let getSiblings = function (e) {
     }
     return siblings;
 };
-
-function select_button(button){
-    var siblings = getSiblings(button);
-    for(var i in siblings){
-        siblings[i].classList.remove("primary-button");
-        siblings[i].classList.add("secondary-button");
-    }
-    button.classList.remove("secondary-button");
-    button.classList.add("primary-button");
-}
-
-function hide_all_pages(container) {
-    var siblings = getSiblings(container);
-    for(var i in siblings){
-        siblings[i].hidden = true;
-    }
-}
-
-function display_page(container, id){
-    var siblings = getSiblings(container);
-    for(var i in siblings){
-        if (siblings[i].id == id){
-            siblings[i].hidden = false;
-        }
-    }
-}
